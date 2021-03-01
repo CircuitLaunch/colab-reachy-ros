c3a761ede906a98ff385ed81f25f0b04428e09ca
Trevor Muraro <4529606+TrevorMuraro@users.noreply.github.com>
2021-03-01 12:08:23 -0800

    Catkin ignores "requires" field in setup.py: https://answers.ros.org/question/229250/catkin_make-not-installing-python-dependencies/

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ from catkin_pkg.python_setup import generate_distutils_setup
 d = generate_distutils_setup(
     packages=['reachy_arm_control'],
     package_dir={'': 'src'},
-    requires=['numpy', 'reachy']
 )
 
 setup(**d)
