8f1c3cd2fb7bd768b141b705e53c88108fca6bea
Trevor Muraro <4529606+TrevorMuraro@users.noreply.github.com>
2021-03-15 16:08:25 -0700

    Listener tested and working!

--- src/speech_listener.py
+++ src/speech_listener.py
@@ -7,9 +7,10 @@ from std_msgs.msg import String
 
 
 class ListenerNode:
-    def __init__(self, name: str, index: Optional[int], frequency: float):
+    def __init__(self, name: str, index: Optional[int], timeout: Optional[int], frequency: float):
         self._device_index = index
         self._recognizer = sr.Recognizer()
+        self._microphone_timeout = timeout
 
         self._publisher = rospy.Publisher(name + "/microphone_speech", String, queue_size=10)
         self._rate = rospy.Rate(frequency)
@@ -21,7 +22,7 @@ class ListenerNode:
             while not rospy.is_shutdown():
                 # Obtain audio from microphone
                 try:
-                    audio = self._recognizer.listen(source, timeout=3)  # TODO: make the tiemout a ROS param?
+                    audio = self._recognizer.listen(source, timeout=self._microphone_timeout)
                 except sr.WaitTimeoutError:
                     rospy.logwarn("No audio heard within timeout")
                 else:
@@ -45,8 +46,9 @@ if __name__ == "__main__":
     # add here the node name. In ROS, nodes are unique named.
     rospy.init_node("speech_listener", log_level=rospy.DEBUG)
     device_index = rospy.get_param("~device_index", default=None)
+    timeout = rospy.get_param("~listen_timeout", default=3)
     listen_frequency = rospy.get_param("~rate", default=10)
 
-    listener = ListenerNode(rospy.get_name(), device_index, listen_frequency)
+    listener = ListenerNode(rospy.get_name(), device_index, timeout, listen_frequency)
 
     listener.spin()
