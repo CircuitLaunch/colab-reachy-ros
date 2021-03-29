2b499e40ebfc245bfe04222871e0608ee030278f
Trevor Muraro <4529606+TrevorMuraro@users.noreply.github.com>
2021-03-29 12:56:52 -0700

    Code formatting cleanup, merged launch file

--- src/speech_speaker.py
+++ src/speech_speaker.py
@@ -1,33 +1,34 @@
 #!usr/bin/env python3
 
+import sys
 import os
 import rospy
 from gtts import gTTS
-from typing import Optional
 from std_msgs.msg import String
 from playsound import playsound
 
 
-
 def speak_callback(msg):
-    language = 'en-us'
-    speech = gTTS(text = msg.data, lang=language, slow=False)
-    filename = 'speech1.mp3'
+    language = "en-us"
+    speech = gTTS(text=msg.data, lang=language, slow=False)
+    filename = "speech1.mp3"
     speech.save(filename)
     playsound(filename)
     os.remove(filename)
     rospy.loginfo("Speaked: %s", msg.data)
 
+
 def main():
-    rospy.init_node('speaker_node')
-    sub=rospy.Subscriber('speak', String, speak_callback)
-    rospy.spin() 
-    
-if __name__=='__main__':
+    rospy.init_node("speaker_node")
+    sub = rospy.Subscriber("speak", String, speak_callback)
+    rospy.spin()
+
+
+if __name__ == "__main__":
     try:
         main()
     except KeyboardInterrupt:
-        print('Interrupted')
+        print("Interrupted")
         os.remove("speech0.mp3")
         os.remove("speech1.mp3")
         try:
@@ -35,4 +36,4 @@ if __name__=='__main__':
         except SystemExit:
             os._exit(0)
 
-    #Add here the name of the ROS. In ROS, names are unique named.
+    # Add here the name of the ROS. In ROS, names are unique named.
