6114938a6eedc8843e8723b594239b9169325be6
Trevor Muraro <4529606+TrevorMuraro@users.noreply.github.com>
2021-03-31 18:22:18 -0700

    Typo fix

--- src/state_machine/helper_functions.py
+++ src/state_machine/helper_functions.py
@@ -4,5 +4,5 @@ from colab_reachy_ros.srv import Speak
 
 def say_something(sentence: str):
     rospy.wait_for_service("/speak")
-    speech_service = rospy.ServiceProxy("/left_arm_controller/set_arm_compliant", Speak)
+    speech_service = rospy.ServiceProxy("/speak", Speak)
     speech_service(sentence)
