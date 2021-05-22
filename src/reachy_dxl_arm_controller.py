46e298e5e66a33bf9c24ed8a567cdb2e6a7424a2
tigakub <tigakub@mac.com>
2021-05-21 19:16:44 -0700

    Still debugging the offsets

--- src/reachy_dxl_arm_controller.py
+++ src/reachy_dxl_arm_controller.py
@@ -344,8 +344,8 @@ class ReachyArmController:
             '''
         dxlIds = [self._dxl_motors[name].id for name in joint_names]
         degrees = [math.degrees(pos) for pos in point.positions]
-        print(f'presentPositions:{[self._dxl_motors[name].presentPosition for name in joint_names]}')
-        print(f'goalPositions:{degrees}')
+        print(f'presentPositions:{dict(zip(joint_names, [self._dxl_motors[name].presentPosition for name in joint_names]))}')
+        print(f'goalPositions:{dict(zip(joint_names, degrees))}')
         # self.u2d2.syncWrite(RAM_GOAL_POSITION, 2, dict(zip(dxlIds, degrees)))
         return True
 
